68c0439656ec072a832b193c1be6b7ba4ff3bbc7
DamianRuma <107467651+DamianRuma@users.noreply.github.com>
2022-06-14 09:02:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+  
+    class Main {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 3, 1};
+
+        int suma = 0;
+
+        for (int i = 0; i <= 2 ; i++) {
+            suma += numbers[i];
+        }
+
+        System.out.println(suma); 
+    }
+
+    }
+  
